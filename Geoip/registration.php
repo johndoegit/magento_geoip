1f82253bbd2975627235f730701a8f246c1e8e87
John Doe <45922882+johndoegit@users.noreply.github.com>
2019-06-10 13:13:20 +0800

    1
    1

---
+++ Geoip/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Above The Fray Design, Inc. All rights reserved.
+ * See ATF_COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ATF_Geoip',
+    __DIR__
+);
